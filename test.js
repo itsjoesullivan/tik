4eeffa5d39caa8a8daad46baed8498572f977641
itsjoesullivan <itsjoesullivan@gmail.com>
2014-10-22 19:23:32 -0400

    req test

--- test.js
+++ test.js
@@ -1,2 +1,11 @@
+var assert = require('assert');
+
 describe('tik', function() {
+});
+
+describe('Req', function() {
+  var Req = require('./lib/req');
+  it('returns a function when passed an object', function() {
+    assert.equal(typeof Req({}), 'function');
+  });
 });
