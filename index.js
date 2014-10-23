4ea89412c0a46bbd3ffaab72264f4de304dcfffd
itsjoesullivan <itsjoesullivan@gmail.com>
2014-10-22 21:06:11 -0400

    tik ls lists tickets.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ var program = require('commander');
 var Req = require('./lib/req');
 var fs = require('fs');
 var path = require('path');
+var charm = require('charm')(process.stdout);
 
 program.version(require('./package').version)
+  .option('ls', 'List issues')
   .option('-i, --identity [path]', 'Auth token (See: https://github.com/settings/tokens/new)')
   .option('-v, --verbose', 'More')
   .option('-l, --label [label]', 'Toggle a label')
@@ -11,7 +13,8 @@ program.version(require('./package').version)
   .option('--remove-label [label-to-remove]', 'Remove a label')
   .parse(process.argv);
 
-var ticketNumber = process.argv.pop();
+
+
 
 /* 
  * Retrieve token if passed arg
@@ -27,13 +30,44 @@ if (program.identity) {
 /*
  * Create a config object
  */
-var config = getRepoInfo();
+var config = getRepoInfo(process.cwd());
 config.token = token || process.env.GITHUB_TOKEN;
 
 /*
  * Initialize the request processor.
  */
 var req = Req(config);
+
+if (process.argv.indexOf('ls') > -1) {
+  req("GET", "/issues", function(err, tickets) {
+    tickets.forEach(function(ticket) {
+      process.stdout.write('#' + ticket.number + ': ' + ticket.title + ' - ' + ticket.user.login);
+      ticket.labels.forEach(function(label) {
+        var labelColor = hex2rgb(label.color).map(function(val) { return val * 0.2; });
+        process.stdout.write(' ');
+        charm.foreground(labelColor[0], labelColor[1], labelColor[2]).write(label.name);
+        charm.foreground(255, 255, 255);
+      });
+    });
+    process.stdout.write('\n');
+  });
+}
+
+/** Return an array of rgb figures from a hex color
+ *
+ * @param {String} hexString
+ * @returns [Array]
+ */
+function hex2rgb(hexString) {
+  return [
+    parseInt(hexString.substring(0,2),16),
+    parseInt(hexString.substring(2,4),16),
+    parseInt(hexString.substring(4,6),16)
+  ]
+}
+
+
+var ticketNumber = getTicketNumber(process.argv);
 
 
 if (ticketNumber) {
@@ -69,25 +103,25 @@ if (ticketNumber) {
   } else {
     describeTicket(ticketNumber);
   }
-} else {
-  req("GET", "/issues", function(err, val) {
-    console.log(val);
-  });
 }
 
 /*
  * Retrieve repo information
+ *
+ * @param {String} dir
+ * @returns {Object}
+ *
  */
-function getRepoInfo() {
-  var wd = process.cwd();
+function getRepoInfo(dir) {
+  dir = dir || process.cwd();
   var gitdir = false;
   var ct = 0;
   while(!gitdir && ct < 20) {
-    var files = fs.readdirSync(wd);
+    var files = fs.readdirSync(dir);
     if(files.indexOf('.git') > -1) {
-      gitdir = wd + '/.git';
+      gitdir = dir + '/.git';
     } else {
-      wd = path.join(wd, '..');
+      dir = path.join(dir, '..');
     }
     ct++;
   }
@@ -138,4 +172,20 @@ function describeTicket(ticketNumber) {
  */
 function hasLabel(ticket, labelName) {
   return !!ticket.labels.filter(function(label) { return label.name === labelName; }).length;
+}
+
+/*
+ * Retrieve a ticket number from an argv, if one is present
+ *
+ * @param {Array} args
+ * @returns {String}
+ *
+ */
+function getTicketNumber(args) {
+  for(var i = 1; i < args.length; i++) {
+    var arg = args[i];
+    if ('' + parseInt(arg) === arg && args[i-1].indexOf('-') !== 0) {
+      return arg;
+    }
+  }
 }
