4c3a3a26aab192690f01b0653d6f18a0efa43b76
itsjoesullivan <itsjoesullivan@gmail.com>
2014-10-29 21:47:10 -0400

    move around so ls is testable

--- index.js
+++ index.js
@@ -2,8 +2,42 @@ var getFirstArg = require('./lib/getFirstArg');
 
 var firstArg = getFirstArg(process.argv);
 
+var program = require('commander');
+
 if (firstArg === 'ls') {
-  require('./ls');
+  var args = process.argv.filter(function(arg) { return arg !== 'ls'; });
+  program.version(require('./package').version)
+    .option('-a, --all', 'Include closed issues')
+    .option('-i, --identity [path]', 'Auth token (See: https://github.com/settings/tokens/new)')
+    .option('-v, --verbose', 'More')
+    .parse(args);
+  /* 
+   * Retrieve token if passed arg
+   */
+  var token;
+  if (program.identity) {
+    var identityContents = fs.readFileSync(program.identity, 'binary');
+    if (identityContents) {
+      token = identityContents;
+    }
+  }
+
+  var getRepoInfo = require('./lib/getRepoInfo');
+  /*
+   * Create a config object
+   */
+  var config = getRepoInfo(process.cwd());
+  config.token = token || process.env.GITHUB_TOKEN;
+
+  /*
+   * Initialize the request processor.
+   */
+  var req = require('./lib/req')(config);
+
+  require('./ls')({
+    program: program,
+    req: req
+  });
 } else if ('' + parseInt(firstArg) === firstArg) {
   require('./ticket');
 } else if (firstArg === 'plumbing') {
--- ls.js
+++ ls.js
@@ -1,64 +1,36 @@
-var program = require('commander');
 var fs = require('fs');
 var path = require('path');
 var charm = require('charm')(process.stdout);
 var moment = require('moment');
 var co = require('co');
 var thunkify = require('thunkify');
-
 var handleError = require('./lib/handleError');
 
-var args = process.argv.filter(function(arg) { return arg !== 'ls'; });
+module.exports = function(obj) { 
 
-program.version(require('./package').version)
-  .option('-a, --all', 'Include closed issues')
-  .option('-i, --identity [path]', 'Auth token (See: https://github.com/settings/tokens/new)')
-  .option('-v, --verbose', 'More')
-  .parse(args);
+  var program = obj.program;
+  var req = obj.req;
 
-/* 
- * Retrieve token if passed arg
- */
-var token;
-if (program.identity) {
-  var identityContents = fs.readFileSync(program.identity, 'binary');
-  if (identityContents) {
-    token = identityContents;
-  }
-}
+  var hex2rgb = require('./lib/hex2rgb');
 
-var getRepoInfo = require('./lib/getRepoInfo');
-/*
- * Create a config object
- */
-var config = getRepoInfo(process.cwd());
-config.token = token || process.env.GITHUB_TOKEN;
+  var issuesPath = '/issues?';
+  if (program.all) issuesPath += 'state=all&';
 
-/*
- * Initialize the request processor.
- */
-var req = require('./lib/req')(config);
-
-var hex2rgb = require('./lib/hex2rgb');
-
-var issuesPath = '/issues?';
-if (program.all) issuesPath += 'state=all&';
-
-
-co(function *(){
-  try {
-    tickets = yield req("GET", issuesPath);
-  } catch(err) {
-    handleError(err);
-  }
-  tickets.forEach(function(ticket) {
-    process.stdout.write('#' + ticket.number + ': ' + ticket.title + ' - ' + ticket.user.login);
-    ticket.labels.forEach(function(label) {
-      var labelColor = hex2rgb(label.color).map(function(val) { return val * 0.2; });
-      process.stdout.write(' ');
-      charm.foreground(labelColor[0], labelColor[1], labelColor[2]).write(label.name);
-      charm.foreground(255, 255, 255);
+  co(function *(){
+    try {
+      tickets = yield req("GET", issuesPath);
+    } catch(err) {
+      handleError(err);
+    }
+    tickets.forEach(function(ticket) {
+      process.stdout.write('#' + ticket.number + ': ' + ticket.title + ' - ' + ticket.user.login);
+      ticket.labels.forEach(function(label) {
+        var labelColor = hex2rgb(label.color).map(function(val) { return val * 0.2; });
+        process.stdout.write(' ');
+        charm.foreground(labelColor[0], labelColor[1], labelColor[2]).write(label.name);
+        charm.foreground(255, 255, 255);
+      });
+      process.stdout.write('\n');
     });
-    process.stdout.write('\n');
-  });
-})();
+  })();
+}
