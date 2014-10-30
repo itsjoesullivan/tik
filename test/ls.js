e9b38c79050c5ced13c63371c7768dfb13994429
itsjoesullivan <itsjoesullivan@gmail.com>
2014-10-29 22:50:24 -0400

    expand a bit

--- test/ls.js
+++ test/ls.js
@@ -3,12 +3,37 @@ var assert = require('assert');
 var exec = require('child_process').exec;
 
 describe('ls', function() {
-  it('returns a list', function(done) {
+  it('returns open tickets', function(done) {
     exec('./tik ls --host http://localhost:4040', function(err, output) {
       var log = output.split('\n');
       assert(log[0].match(/#1/));
       assert(log[1].match(/#2/));
+      assert(!log[2].match(/#3/));
       done();
     });
   });
+  describe('-a, --all', function() {
+    describe('--all', function() {
+      it('returns all tickets', function(done) {
+        exec('./tik ls --host http://localhost:4040 --all', function(err, output) {
+          var log = output.split('\n');
+          assert(log[0].match(/#1/));
+          assert(log[1].match(/#2/));
+          assert(log[2].match(/#3/));
+          done();
+        });
+      });
+    });
+    describe('-a', function() {
+      it('-a returns all tickets', function(done) {
+        exec('./tik ls --host http://localhost:4040 --all', function(err, output) {
+          var log = output.split('\n');
+          assert(log[0].match(/#1/));
+          assert(log[1].match(/#2/));
+          assert(log[2].match(/#3/));
+          done();
+        });
+      });
+    });
+  })
 });
--- test/server.js
+++ test/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/repos/itsjoesullivan/tik/issues', function(req, res) {
-  res.json([
+  var tickets = [ 
     {
       number: 1,
       title: "title",
@@ -28,8 +28,26 @@ app.get('/repos/itsjoesullivan/tik/issues', function(req, res) {
           color: "#000000"
         }
       ]
+    },
+    {
+      number: 3,
+      title: "title",
+      user: {
+        login: "user",
+      },
+      labels: [
+        {
+          name: "label-name",
+          color: "#000000"
+        }
+      ]
     }
-  ]);
+  ]
+  if (req.query.state === 'all') {
+    res.json(tickets);
+  } else {
+    res.json(tickets.slice(0,2));
+  }
 });
 
 app.listen(4040);
