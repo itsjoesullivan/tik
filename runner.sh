7f4c5e7e999f105235f24b13fcf61e01e01fd35d
itsjoesullivan <itsjoesullivan@gmail.com>
2014-11-01 18:06:40 -0400

    init ticket testing

--- runner.sh
+++ runner.sh
@@ -1,6 +1,9 @@
 node test/server &
 SERVER_PID=$!
 
+echo "Testing 'tik {ticket-number}'"
+mocha test/ticket
+echo "Testing 'tik ls'"
 mocha test/ls
 
 kill $SERVER_PID
---
+++ test/ticket.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+
+var exec = require('child_process').exec;
+
+describe('ticket', function() {
+  it('describes a ticket', function(done) {
+    exec('./tik 1 --host http://localhost:4040', function(err, output) {
+      var lines = output.split('\n');
+      assert(/#[0-9]/.test(lines[0]));
+      assert(/title/i.test(lines[1]));
+      assert(/author/i.test(lines[2]));
+      assert(/assignee/i.test(lines[3]));
+      done();
+    });
+  });
+  describe('--comments', function() {
+    it('includes comment count', function(done) {
+      exec('./tik 1 --comments --host http://localhost:4040', function(err, output) {
+        var lines = output.split('\n');
+        assert(/Comments \([0-9]+\)/.test(output));
+        done();
+      });
+    });
+  });
+});
